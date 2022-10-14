cca3224a3d1d6e8b26b25bbbe59c782fb44dd257
Narduk <narduk.a@gmail.com>
2022-10-13 22:52:57 -0500

    Actualización 13 oct

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-/*onclick = */function validar() {
-    var usuario = document.getElementById("usuario").value;
-    var Contraseña = document.getElementById("pass").value;
+/*onclick = */
+function validar() {
+  var usuario = document.getElementById("usuario").value;
+  var Contraseña = document.getElementById("pass").value;
   
-    if (usuario == "admin" && Contraseña == "1234") {
-      //alert("Usuario y Contraseña validos");
-      window.location = "ventas.html";
-    } else {
-      alert("Verifique sus credenciales");
-    }
+  if (usuario == "admin" && Contraseña == "1234") {
+    //alert("Usuario y Contraseña validos");
+    window.location = "ventas.html";
+  } else {
+    alert("Verifique sus credenciales");
   }
+}
 
-import { teatros, teatros } from "./teatros";
+function algoritmo() {
+  let clientes_atendidos;
+  let total_de_la_compra, total_de_ventas;
+  total_de_la_compra = parseFloat(
+    document.formulario1.total_de_la_compra.value
+  );
+  total_de_ventas = total_de_ventas + total_de_la_compra;
+  clientes_atendidos = parseInt(clientes_atendidos) + 1;
+  alert(clientes_atendidos);
+}
+
+/*import { teatros, teatros } from "./teatros";
 
 const disponibilidadTeatros = Object.keys(teatros);
 const targetTeatro = prompt(`¿En que cinema desea alquirir su boleta? ${disponibilidadTeatros}`);
@@ -27,7 +39,7 @@ alert(`El precio a pagar por ${quantity} es: $ ${total}`);
 } else {
 alert("Cinema incorrecto");
 }
-
+*/
   /*
   function Login() {
     var done = 0;
